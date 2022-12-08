9cb0d22461b17b901c1fd188f6130f48cb03661c
Bhushanab159 <106249755+Bhushanab159@users.noreply.github.com>
2022-12-08 21:26:58 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,11 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React from "react";
 
-class App extends Component {
-    render() {
-
-        return(
-            <div id="main">
-               {/* Do not remove the main div */}
-               <ol key="relativeList">
-                <li key="relativeListItem1">Shraddha Tiwari</li>
-                <li key="relativeListItem1">Raksha Tiwari</li>
-                <li key="relativeListItem1">Sandeep</li>
-               </ol>
-            </div>
-        )
-    }
+const App = () => {
+return (
+    <div>
+        <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+    </div>
+  )
 }
-
 
 export default App;
